9feec7053d01130fb871916e89d77570f0ca7a92
Sandeep <sandeepkharoundha@gmail.com>
2026-07-06 11:40:15 +0530

    restyle the pdf like a latex paper (serif prose, mono data)

--- build/build.js
+++ build/build.js
@@ -3,15 +3,20 @@ const path = require('node:path');
 const { PdfWriter } = require('./pdf-writer');
 const { buildBundle } = require('./bundle');
 
+const L = 72;    // left margin
+const R = 540;   // right margin
+const W = 612;   // page width
+
 function textField(w, name, rect, opts) {
   opts = opts || {};
   let ff = 0;
   if (opts.multiline) ff |= 4096;
   if (opts.readonly) ff |= 1;
   const v = opts.value ? w.pdfString(opts.value) : '()';
+  const size = opts.size || 10;
   let s = '<< /Type /Annot /Subtype /Widget /FT /Tx /T ' + w.pdfString(name) +
-    ' /Ff ' + ff + ' /Rect [' + rect.join(' ') + '] /V ' + v + ' /DA (/Helv 11 Tf 0 g)' +
-    ' /MK << /BC [0.6 0.6 0.6] /BG [1 1 1] >> /BS << /W 1 /S /S >>';
+    ' /Ff ' + ff + ' /Rect [' + rect.join(' ') + '] /V ' + v + ' /DA (/Cour ' + size + ' Tf 0.1 0.1 0.1 rg)' +
+    ' /MK << /BC [0.55 0.55 0.55] /BG [0.99 0.99 0.98] >> /BS << /W 0.6 /S /S >>';
   if (opts.hidden) s += ' /F 2';
   return w.add(s + ' >>');
 }
@@ -20,49 +25,65 @@ function button(w, name, rect, caption, js) {
   return w.add(
     '<< /Type /Annot /Subtype /Widget /FT /Btn /Ff 65536 /T ' + w.pdfString(name) +
     ' /Rect [' + rect.join(' ') + '] /MK << /CA ' + w.pdfString(caption) +
-    ' /BG [0.86 0.86 0.86] /BC [0.4 0.4 0.4] >> /BS << /W 1 /S /S >>' +
+    ' /BG [0.92 0.91 0.89] /BC [0.45 0.45 0.45] >> /BS << /W 0.8 /S /S >>' +
     ' /A << /S /JavaScript /JS ' + w.pdfString(js) + ' >> >>'
   );
 }
 
 function build() {
   const w = new PdfWriter();
-  const font = w.add('<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>');
-  const bold = w.add('<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica-Bold >>');
+  const times = w.add('<< /Type /Font /Subtype /Type1 /BaseFont /Times-Roman >>');
+  const timesBold = w.add('<< /Type /Font /Subtype /Type1 /BaseFont /Times-Bold >>');
+  const timesItalic = w.add('<< /Type /Font /Subtype /Type1 /BaseFont /Times-Italic >>');
+  const courier = w.add('<< /Type /Font /Subtype /Type1 /BaseFont /Courier >>');
 
   const fields = [];
   fields.push(textField(w, 'state', [0, 0, 1, 1], { hidden: true }));
-  fields.push(textField(w, 'todo_list', [40, 500, 572, 712], { multiline: true, readonly: true }));
-  fields.push(textField(w, 'new_todo', [40, 456, 380, 484], {}));
-  fields.push(button(w, 'add', [390, 456, 480, 484], 'Add', 'onAdd();'));
-  fields.push(textField(w, 'action_id', [40, 410, 130, 438], {}));
-  fields.push(button(w, 'toggle', [140, 410, 280, 438], 'Toggle done', 'onToggle();'));
-  fields.push(button(w, 'delete', [290, 410, 390, 438], 'Delete', 'onDelete();'));
-  fields.push(textField(w, 'sql_input', [40, 276, 572, 342], { multiline: true, value: 'select * from todos;' }));
-  fields.push(button(w, 'run', [40, 244, 170, 272], 'Run SQL', 'onRunSql();'));
-  fields.push(textField(w, 'sql_output', [40, 40, 572, 224], { multiline: true, readonly: true }));
+  fields.push(textField(w, 'todo_list', [L, 486, R, 670], { multiline: true, readonly: true }));
+  fields.push(textField(w, 'new_todo', [L, 446, 400, 470], {}));
+  fields.push(button(w, 'add', [410, 446, 500, 470], 'Add', 'onAdd();'));
+  fields.push(textField(w, 'action_id', [L, 408, 160, 432], {}));
+  fields.push(button(w, 'toggle', [170, 408, 300, 432], 'Toggle done', 'onToggle();'));
+  fields.push(button(w, 'delete', [310, 408, 410, 432], 'Delete', 'onDelete();'));
+  fields.push(textField(w, 'sql_input', [L, 268, R, 348], { multiline: true, size: 11, value: 'select * from todos;' }));
+  fields.push(button(w, 'run', [L, 236, 200, 262], 'Run SQL', 'onRunSql();'));
+  fields.push(textField(w, 'sql_output', [L, 44, R, 206], { multiline: true, readonly: true }));
 
   const refs = fields.map(n => n + ' 0 R').join(' ');
 
-  const labels = [
-    { size: 20, y: 760, text: 'backend.pdf', bold: true },
-    { size: 11, y: 740, gray: 0.35, text: 'a real SQL database that lives inside this PDF file' },
-    { size: 12, y: 720, bold: true, text: 'Your todos' },
-    { size: 10, y: 490, gray: 0.35, text: 'Add a todo:' },
-    { size: 9, y: 446, gray: 0.4, text: 'Toggle or delete by id - type the number, then click the button' },
-    { size: 9, y: 384, gray: 0.4, text: 'To save your data: click the Download icon in the toolbar (Cmd+S just re-downloads the original)' },
-    { size: 12, y: 370, bold: true, text: 'SQL console' },
-    { size: 9, y: 356, gray: 0.35, text: 'Edit the query and hit Run SQL. Try: select text from todos where done = 0' },
-    { size: 12, y: 234, bold: true, text: 'Results' },
-  ];
+  // fonts for drawn text: Ti = Times, TiBd = bold, TiIt = italic, Cour = mono
+  const F = { times: 'Ti', bold: 'TiBd', italic: 'TiIt', mono: 'Cour' };
+  const widthFactor = { Ti: 0.5, TiBd: 0.52, TiIt: 0.5, Cour: 0.6 };
 
   let content = '';
-  for (const l of labels) {
-    const g = l.gray == null ? 0 : l.gray;
-    const fontName = l.bold ? 'HelvB' : 'Helv';
+  function draw(font, size, x, y, str, gray) {
+    const g = gray == null ? 0 : gray;
     content += g + ' ' + g + ' ' + g + ' rg\n';
-    content += 'BT /' + fontName + ' ' + l.size + ' Tf ' + 40 + ' ' + l.y + ' Td ' + w.pdfString(l.text) + ' Tj ET\n';
+    content += 'BT /' + font + ' ' + size + ' Tf ' + x + ' ' + y + ' Td ' + w.pdfString(str) + ' Tj ET\n';
   }
+  function centered(font, size, y, str, gray) {
+    const x = (W - str.length * size * widthFactor[font]) / 2;
+    draw(font, size, x, y, str, gray);
+  }
+  function rule(y) {
+    content += '0.6 0.6 0.6 RG 0.6 w ' + L + ' ' + y + ' m ' + R + ' ' + y + ' l S\n';
+  }
+
+  centered(F.bold, 24, 744, 'backend.pdf', 0.05);
+  centered(F.italic, 12.5, 723, 'A relational database that lives inside this very document', 0.2);
+  centered(F.mono, 9, 707, 'no server    no dependencies    one file', 0.4);
+  rule(697);
+
+  draw(F.bold, 13, L, 676, '1    Your todos', 0.05);
+  draw(F.italic, 10.5, L, 474, 'Add a todo:', 0.3);
+  draw(F.italic, 9.5, L, 438, 'Toggle or delete a todo by its id - type the number, then click.', 0.35);
+  draw(F.italic, 9.5, L, 392, 'To save your data, use the Download icon in the toolbar (Cmd+S only re-downloads the original).', 0.35);
+
+  draw(F.bold, 13, L, 372, '2    SQL console', 0.05);
+  draw(F.italic, 9.5, L, 356, 'Edit the query and press Run SQL. Try:  select text from todos where done = 0', 0.35);
+
+  draw(F.bold, 13, L, 216, '3    Results', 0.05);
+
   const contentStream = w.addStream('<< >>', content);
 
   // spike: only /OpenAction fires on load in chrome, not the js name tree
@@ -73,14 +94,15 @@ function build() {
   const pages = w.alloc();
   const catalog = w.alloc();
 
-  const resources = '<< /Font << /Helv ' + font + ' 0 R /HelvB ' + bold + ' 0 R >> >>';
+  const resources = '<< /Font << /Ti ' + times + ' 0 R /TiBd ' + timesBold + ' 0 R /TiIt ' +
+    timesItalic + ' 0 R /Cour ' + courier + ' 0 R >> >>';
   w.set(page,
     '<< /Type /Page /Parent ' + pages + ' 0 R /MediaBox [0 0 612 792]' +
     ' /Resources ' + resources + ' /Contents ' + contentStream + ' 0 R /Annots [' + refs + '] >>');
   w.set(pages, '<< /Type /Pages /Kids [' + page + ' 0 R] /Count 1 >>');
   w.set(catalog,
     '<< /Type /Catalog /Pages ' + pages + ' 0 R' +
-    ' /AcroForm << /Fields [' + refs + '] /NeedAppearances true /DA (/Helv 11 Tf 0 g)' +
+    ' /AcroForm << /Fields [' + refs + '] /NeedAppearances true /DA (/Cour 10 Tf 0 g)' +
     ' /DR ' + resources + ' >>' +
     ' /OpenAction ' + openAction + ' 0 R >>');
   w.setRoot(catalog);
